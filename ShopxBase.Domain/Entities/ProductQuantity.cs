018bcc830c1d77f1061af50afdc5fd81165c708b
Hungztv <your-email@example.com>
2026-01-08 11:03:38 +0700

    Create entity: Shipping

--- ShopxBase.Domain/Entities/ProductQuantity.cs
+++ ShopxBase.Domain/Entities/ProductQuantity.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Shopping.Domain.Entities
+namespace ShopxBase.Domain.Entities
 {
     public class ProductQuantity : BaseEntity
     {
