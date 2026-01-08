f5ea349188ece999a6767b8fda8ceafeeb969820
Hungztv <your-email@example.com>
2026-01-08 11:00:54 +0700

    Create entity: Contact

---
+++ ShopxBase.Domain/Entities/Contact.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopxBase.Domain.Entities
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Map { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string LogoImg { get; set; }
+
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string Twitter { get; set; }
+        public string Youtube { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
